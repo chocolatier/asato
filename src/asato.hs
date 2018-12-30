9c8225aabccc0503830f35812f4f3b8335a10f66
chocolatier <chocolatier@users.noreply.github.com>
2018-12-30 12:30:35 +1100

    test code

--- src/asato.hs
+++ src/asato.hs
@@ -1,6 +1,10 @@
-module Lib
-    ( someFunc
-    ) where
+import Network.API.TheMovieDB
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+main :: IO ()
+main = do
+  -- The API key assigned to you (as a 'Text' value).
+  let key = "your API key"
+  -- The 'fetch' function will get a 'Movie' record based on its ID.
+  result <- runTheMovieDB key (fetchMovie 9340)
+  -- Do something with the result (or error).
+  putStrLn (show result)
